400117fd00ceaa1a570f5069b09d7c013f9ba690
shivgopal <shiv20@live.dk>
2021-11-04 11:45:49 +0100

    ..

---
+++ src/main/webapp/Script.js
@@ -0,0 +1,8 @@
+let Brugernavn = document.getElementById('InputBrugernavn')
+let Kode = document.getElementById('InputKode')
+
+function LoginValidation(){
+    if (Brugernavn != null && Kode != null){
+
+    }
+}
