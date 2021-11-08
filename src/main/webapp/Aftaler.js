d4898681cac1d56951ba193b670753fcbaaded78
shivgopal <shiv20@live.dk>
2021-11-08 19:34:40 +0100

    Aftale + konsultationer

--- src/main/webapp/Aftaler.js
+++ src/main/webapp/Aftaler.js
@@ -11,9 +11,14 @@ function AftaleOprettelse(){
     let Sluttid = document.getElementById("tid_til").value;
     let CPR = document.getElementById("CPR").value;
 
-    document.getElementById("1").innerHTML = Dato;
-    document.getElementById("2").innerHTML = StartTid;
-    document.getElementById("3").innerHTML = Sluttid;
-    document.getElementById("4").innerHTML = CPR;
+    if (Dato != null && StartTid != null && Sluttid != null && CPR.length == 10){
+        document.getElementById("1").innerHTML = Dato;
+        document.getElementById("2").innerHTML = StartTid;
+        document.getElementById("3").innerHTML = Sluttid;
+        document.getElementById("4").innerHTML = CPR;
+    } else{
+        document.getElementById("1").innerHTML = "12";
+        alert("Indtast venligst alle oplsyninger. ")
+    }
 
 }
