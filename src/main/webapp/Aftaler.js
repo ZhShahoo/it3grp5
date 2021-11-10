f480eae55c8566e8d90b1a2d5435c7b3c6d46fc7
shivgopal <shiv20@live.dk>
2021-11-10 13:46:14 +0100

    Aftale + konsultationer

--- src/main/webapp/Aftaler.js
+++ src/main/webapp/Aftaler.js
@@ -1,4 +1,5 @@
 BrugerFremvsinging();
+let i = 1;
 
 function BrugerFremvsinging() {
     Bruger = sessionStorage.getItem("Godkendt_Bruger");
@@ -10,17 +11,25 @@ function AftaleOprettelse(){
     let StartTid = document.getElementById("tid_fra").value;
     let Sluttid = document.getElementById("tid_til").value;
     let CPR = document.getElementById("CPR").value;
+    let SP = document.getElementById("SP").value;
 
 
 
-    if (Dato != null && StartTid != null && Sluttid != null && CPR.length == 10){
-        document.getElementById("1").innerHTML = Dato;
-        document.getElementById("2").innerHTML = StartTid;
-        document.getElementById("3").innerHTML = Sluttid;
-        document.getElementById("4").innerHTML = CPR;
+    if (Dato != null && StartTid != null && Sluttid != null && CPR.length == 10 && SP != null){
+        document.getElementById("Dato"+i).innerHTML = Dato;
+        document.getElementById("Starttid"+i).innerHTML = StartTid;
+        document.getElementById("Sluttid"+i).innerHTML = Sluttid;
+        document.getElementById("CPR"+i).innerHTML = CPR;
+        document.getElementById("SP"+i).innerHTML = SP;
+        i++;
+
     } else{
-        document.getElementById("1").innerHTML = "12";
         alert("Indtast venligst alle oplsyninger. ")
     }
+}
 
+function Clear(){
+    for (){
+
+    }
 }
