89cf271041f1b357f72d6db7d5bd2c8a39670d38
ZAkir Hussain Shahoo <s194054@student.dtu.dk>
2021-11-18 10:43:24 +0100

    Virker men navn skal skiftes

--- src/main/java/rest/Aftaler.java
+++ src/main/java/rest/Aftaler.java
@@ -8,7 +8,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
-import static rest.GiraffeDao.*;
+import static rest.PatientDAO.*;
 
 
 @Path("Aftaler")
@@ -19,14 +19,14 @@ import static rest.GiraffeDao.*;
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Patient> getGiraffes() {
-        return getInstance().getGiraffes();
+        return getInstance().getPatienter();
     }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     public Patient postPatient(Patient p) {
 
-        getInstance().getGiraffes().add(p);
+        getInstance().getPatienter().add(p);
 
 
         System.out.println("dato: " + p.dato );
@@ -36,9 +36,9 @@ import static rest.GiraffeDao.*;
         System.out.println("slut: " + p.slutTidspunkt );
         System.out.println("CPR: " + p.notater );
 
-        if (p.dato != null && p.CPR != null && p.name != null && p.startTidspunkt != null && p.slutTidspunkt != null && p.notater != null){
+
             sql.opretNyPatient(p.CPR,p.dato,p.startTidspunkt,p.slutTidspunkt,p.name,p.name,p.notater);
-        }
+
         return p;
         }
     }
